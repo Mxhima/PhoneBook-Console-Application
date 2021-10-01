1d3a8c3008bf9ab0cd9566756f47fc3393154667
Ishan-Chanuka <ishanchanuka4@gmail.com>
2021-10-01 21:57:12 +0530

    Operations added

--- Phonebook Console Application/Program.cs
+++ Phonebook Console Application/Program.cs
@@ -7,7 +7,29 @@ namespace Phonebook_Console_Application
         static void Main(string[] args)
         {
             //Operations 
-            
+            Console.WriteLine("Hello this is my Phonebook console program");
+
+            Console.WriteLine("Select the Operation");
+
+            Console.WriteLine("1 Add Contact");
+
+            Console.WriteLine("2 Display Contact by Number");
+
+            Console.WriteLine("3 View all Contacts");
+
+            Console.WriteLine("4 Search for contacts for a given name");
+
+            Console.WriteLine("Press 'x' to exit");
+
+            Console.WriteLine("");
+
+
+
+            var UserInput = Console.ReadLine();
+
+
+
+            var PhoneBook = new Phonebook();
 
             //Operation Outputs
             while (true)
